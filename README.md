a230741e180768a91f810263b158963fa0880364
shinokada <okada.shin@gmail.com>
2013-11-25 07:26:27 +0900

    added app1a.rb

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Command line app
+
+## Usage
+
+app.rb
+
+    # 1. 
+    $ ruby app.rb oui
+    # 2. 
+    $ ruby app.rb list_recipes --format oneline
+    # or
+    $ ruby app.rb list_recipes --format "oneline"
+    # or
+    $ ruby app.rb list_recipes --format="oneline"
+
+    # 3.
+    $ ruby app.rb list_recipes --format="oneline" --no-show-time
+
+app1a.rb
+
+Addition to app.rb
+    # 4. uby app1a.rb list_recipes --genre French --format oneline --no-show-time
+
+app2.rb
+
+    # 1. 
+    $ ruby app2.rb recipes add # this will give an error
+
+    # 2. 
+    $ ruby app2.rb recipes add --title="Steak" --cooking-time="10 min" --description="Good ol' steak"
+
+
+app3.rb
+
+    # 1. 
+    $ ruby app3.rb recipes list
+
+    # 2. This will show help message for recipes.
+    $ ruby app3.rb recipes
+
+app4.rb
+
+    # 1. To display help
+    $ ruby app4.rb recipes
+
+    # 2.
+    $ ruby app4.rb recipes add -c="10 min" -t "Steak" -d "Good ol' steak."
+ 
---
+++ recipes.rb
@@ -0,0 +1,91 @@
+RECIPES = [
+  {
+    title: "Ratatouille",
+    cooking_time: "60 min",
+    genre: "French",
+    ingredients:  %w(potatoes carrots peppers onions zucchini tomatoes)
+  },
+  {
+    title: "Mac & Cheese",
+    cooking_time: "20 min",
+    genre: '',
+    ingredients: %w(macarroni cheese mustard milk)
+  },
+  {
+    title: "Sushi",
+    cooking_time: "30 min",
+    genre:'Japanese',
+    ingredients:  %w(2 cups sushi or short grain rice
+2 cups water, plus extra for rinsing rice
+2 tablespoons rice vinegar
+2 tablespoons sugar
+1 tablespoon kosher)
+  },
+  {
+    title: "Baked Chicken and Pastina",
+    cooking_time: "35 min",
+    genre: 'Italian',
+    ingredients: %w(1 cup pastina pasta \(or any small pasta\)
+2 tablespoons olive oil
+1/2 cup cubed chicken breast \(1-inch cubes\)
+1/2 cup diced onion \(about 1/2 a small onion\)
+1 clove garlic, minced
+1 \(14.5-ounce\) can diced tomatoes with juice
+1 cup shredded mozzarella
+1/4 cup chopped fresh flat-leaf parsley
+1/4 teaspoon kosher salt
+1/4 teaspoon freshly ground black pepper
+1/4 cup bread crumbs
+1/4 cup grated Parmesan
+1 tablespoon butter, plus more for buttering the baking dish)
+  },
+  {
+    title: "Chinese Salad",
+    cooking_time: "20 min",
+    genre: 'Chinese',
+    ingredients: %w(1 head Napa or bok choy cabbage
+3 green onions
+1 \(3-ounce\) package ramen noodles with seasoning pack
+1 \(6-ounce\) package slivered almonds
+1 tablespoon sesame seeds
+1/2 cup vegetable oil
+1/4 cup sugar
+3 tablespoons white vinegar
+1/4 teaspoon salt)
+  },
+  {
+    title: "Italian Wedding Soup",
+    cooking_time: "30 min",
+    genre: 'Italian',
+    ingredients: %w(12 cups low-sodium chicken broth
+1 pound curly endive, coarsely chopped \(1 pound of escarole would be a good substitution\)
+2 large eggs
+2 tablespoon freshly grated Parmesan, plus extra for garnish
+Salt and freshly ground black pepper)
+  },
+  {
+    title: "Christmas cake",
+    cooking_time: "40 min",
+    genre: 'Christmas',
+    ingredients: %w(1/2 pound unsalted butter
+1/2 pound light brown sugar
+1 teaspoon vanilla extract
+2 teaspoons lime juice
+1 teaspoon lime zest
+6 eggs
+6 ounces fresh bread crumbs
+6 ounces flour
+1/2 teaspoon ground cinnamon
+1/2 teaspoon ground nutmeg
+1/2 teaspoon ground ginger
+1/4 teaspoon ground cloves
+1 1/2 teaspoons baking powder
+1/4 teaspoon kosher salt
+1 cup rum
+1 pint Port
+4 cups mixed marinated fruit, recipe follows, squeezed and finely chopped)
+ }
+
+
+
+]
